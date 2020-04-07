20e181d4fe277fc11861a920bdbe887c02f21d5a
Tuan Pham <tuanpham.developer@gmail.com>
2020-04-07 19:17:15 -0400

    Updates

---
+++ precache-manifest.8fd9a34be3c0a96400a7fc89b4187798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d43eebf06075bfe5a6c40032faa288c",
+    "url": "/youtube-loop-react-redux-class-component/index.html"
+  },
+  {
+    "revision": "6187b385a01f201cce5c",
+    "url": "/youtube-loop-react-redux-class-component/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "f747c6e0d30b0197904c",
+    "url": "/youtube-loop-react-redux-class-component/static/css/main.46ae43d3.chunk.css"
+  },
+  {
+    "revision": "6187b385a01f201cce5c",
+    "url": "/youtube-loop-react-redux-class-component/static/js/2.f4438970.chunk.js"
+  },
+  {
+    "revision": "73b207d866c46f1507394f1c54d6ac97",
+    "url": "/youtube-loop-react-redux-class-component/static/js/2.f4438970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f747c6e0d30b0197904c",
+    "url": "/youtube-loop-react-redux-class-component/static/js/main.60e943cc.chunk.js"
+  },
+  {
+    "revision": "6b243e2729dd6b23e4b2",
+    "url": "/youtube-loop-react-redux-class-component/static/js/runtime-main.f4a0a915.js"
+  }
+]);
